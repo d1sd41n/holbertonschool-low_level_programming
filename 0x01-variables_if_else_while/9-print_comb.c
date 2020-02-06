6b28e423d992df734af1b7e9c6dc2dce48871307
Daniel <outnumbered@live.com>
2020-02-06 20:28:15 +0000

    ssdss

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		if (i < '9')
-		{
+		if (i == '9')
+			break;
 		putchar(',');
 		putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);
