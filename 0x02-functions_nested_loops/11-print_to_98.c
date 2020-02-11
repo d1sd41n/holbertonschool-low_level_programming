760ef6a90400895a981453208c08fd88623b7fd7
Daniel <outnumbered@live.com>
2020-02-11 19:10:36 +0000

    ssdss

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,42 +11,21 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			if (n < 10 && n >= 0)
-			{
-			_putchar(n + '0');
-			}
-			else if ( n < 0 && n > -10)
-			{
-				_putchar('-');
-				_putchar(n * - 1 + '0');
-			}
-			else if ( n < 0 )
-                        {
-                                _putchar('-');
-                                _putchar(n / 10 * - 1 + '0');
-				_putchar(n % 10 * - 1 + '0');
-                        }
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if ( n == 98)
+			printf("%d", n);
+			if (n == 98)
 				break;
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
 	}
 	else
 	{
 		for (n = n; n >= 98; n--)
 		{
-			printf("%i", n);
-			if ( n == 98)
+			printf("%d", n);
+			if (n == 98)
 				break;
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
