606ac859a87f90f4ea899a97747eb43eb1cee1a0
Daniel <outnumbered@live.com>
2020-02-11 22:02:07 +0000

    ssdss

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  * main - Entry point
@@ -8,11 +7,11 @@
 int main(void)
 {
 	int i, sum;
-	
+
 	sum = 0;
 	for (i = 1; i < 1024 ; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
 
