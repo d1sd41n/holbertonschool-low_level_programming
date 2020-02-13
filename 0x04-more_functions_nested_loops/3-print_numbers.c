e0d5c10be2ae590c6c604d9e0ea8d1b7fbedb527
Daniel <outnumbered@live.com>
2020-02-13 15:24:00 +0000

    ssdss

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - writes numbers
+ *
+ * Return: none.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
