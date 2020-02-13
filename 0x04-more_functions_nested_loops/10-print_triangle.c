0715625bc9e86d9684fe2373ffb608aeb0ae5ce2
Daniel <outnumbered@live.com>
2020-02-13 20:36:37 +0000

    ssdss

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_triangle - writes numbers
+ * @size: d
+ * Return: none.
+ */
+void print_triangle(int size)
+{
+	int r, c;
+
+	if (size > 0)
+	{
+		for (r = 1; r <= size; r++)
+		{
+			for (c = 1; c <= size; c++)
+			{
+				if (size - r >= c)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
