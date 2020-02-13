4cebf0669941de5acd903986d61307f80ca1775d
Daniel <outnumbered@live.com>
2020-02-13 19:22:21 +0000

    ssdss

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_square - writes numbers
+ *
+ * Return: none.
+ */
+void print_square(int size)
+{
+	int r, c;
+
+	if (size > 0)
+	{
+		for (r = 0; r < size; r++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
