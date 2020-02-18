3d737cd91fcc5cbb02fdaa6542da9ef86e917dd7
Daniel <outnumbered@live.com>
2020-02-18 18:30:21 +0000

    ssdss

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_rev - wricdwcwc
+ * @s: pointer
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (1)
+	{
+		if (s[i] == '\0')
+			break;
+		i += 1;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
