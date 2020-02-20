c7150fcd332afc8c039b4c2c4ef51b402bc0f0ed
Daniel <outnumbered@live.com>
2020-02-20 19:33:14 +0000

    answer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,14 +12,14 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (src[i]=='\0')
+		if (src[i] == '\0')
 		{
 			dest[i] = '\0';
-			break;			
+			break;
 		}
 		dest[i] = src[i];
 	}
-	 for ( ; i < n; i++)
-                   dest[i] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
