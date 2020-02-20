7d7ce0e4100ee907598c8b35a640835a697817d2
Daniel <outnumbered@live.com>
2020-02-20 21:35:46 +0000

    answer

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *string_toupper - writes nudscfdsmbers
+ * @str: pointer
+ * Return: none
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
