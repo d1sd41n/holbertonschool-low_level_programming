70185d3256a614ae0ae90c5fb0231d515d8a555c
Daniel <outnumbered@live.com>
2020-02-20 20:54:39 +0000

    answer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,18 +10,15 @@ void reverse_array(int *a, int n)
 	int a2[1000];
 	int j, k;
 
-	k = 0;
-
-	k = n;
-	for (j = 0; j <= n; j++)
+	k = n - 1;
+	for (j = 0; j < n; j++)
 	{
 		a2[j] = a[k];
 		k -= 1;
 	}
 
-	a2[j] = '\0';
 	k = 0;
-	while (a2[k] != '\0')
+	while (k < n)
 	{
 		a[k] = a2[k];
 		k += 1;
