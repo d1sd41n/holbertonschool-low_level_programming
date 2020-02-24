ff9a36361c4e0ee663f6a2db1d9b2d02946f3753
Daniel <outnumbered@live.com>
2020-02-24 21:33:07 +0000

    automated-commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * *_strstr - locates a substring
+ * @haystack: pointer to string
+ * @needle: pointer to string
+ * Return: char pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k, f;
+	char *p = 0;
+
+	i = 0;
+	j = 0;
+	f = 0;
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			k = i;
+			while (needle[j] != '\0')
+			{
+				if (needle[j] != haystack[k])
+				{
+					f = 1;
+					break;
+				}
+				k++;
+				j++;
+			}
+			if (f != 1)
+			{
+				p = &haystack[i];
+				break;
+			}
+			k = 0;
+			f = 0;
+			j = 0;
+		}
+		i++;
+	}
+	return (p);
+}
