89363b6a98a4a7ca3583559a53c8ba2942399833
Daniel <outnumbered@live.com>
2020-02-26 21:42:01 +0000

    automated-commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * is_prime_number - return 1 if n is prime number
+ * @n: int
+ * Return: int
+ */
+int is_prime(int i, int n)
+{
+	if (n == i)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else
+		return (is_prime(i + 1, n));
+}
+/**
+ * is_prime_number - return 1 if n is prime number
+ * @n: int
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	return (is_prime(2, n));
+}
