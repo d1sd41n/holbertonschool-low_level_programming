88af508fd50045f219ae480ecf0f24f2e5410171
Daniel <outnumbered@live.com>
2020-02-26 19:31:27 +0000

    automated-commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - poweres a number
+ * @x: number
+ * @y: number
+ * Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
