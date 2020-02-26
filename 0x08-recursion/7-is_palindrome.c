a7bfb4673f22f448c615be91365c35f8b0e36ec3
Daniel <outnumbered@live.com>
2020-02-26 23:19:16 +0000

    automated-commit

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,18 +1,43 @@
 #include "holberton.h"
 /**
- * puts_half - writes nudscfdsmbers
- * @str: pointer
- * Return: none
+ * _strlen_recursion - writes nudscfdsmbers
+ * @s: pointer
+ * Return: len
  */
-int size(char *s)
+int _strlen_recursion(char *s)
 {
-	int i;
-
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
 }
 
+/**
+ * compare - writes nudscfdsmbers
+ * @s: pointer
+ * @i: int
+ * @f: int
+ * Return: 1 or 0
+ */
+int compare(char *s, int i, int f)
+{
+	if (s[i] != s[f])
+		return (0);
+	else if (i <= f)
+		return (compare(s, i + 1, f - 1));
+	else
+		return (1);
+}
+
+/**
+ * is_palindrome - writes nudscfdsmbers
+ * @s: pointer
+ * Return:intt
+ */
 int is_palindrome(char *s)
 {
-	int s;
+	int strl;
 
-	s = size(s);
+	strl = _strlen_recursion(s) - 1;
+	return (compare(s, 0, strl));
 }
