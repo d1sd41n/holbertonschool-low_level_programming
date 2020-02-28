798cfe5b0525005231b3a4cacc9d7660cd5de049
Daniel <outnumbered@live.com>
2020-02-28 23:20:41 +0000

    automated-commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: entry n
+ * @argv: entry array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	mul = 0;
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i\n", mul);
+	return (0);
+}
