e4c2bd9bde1c69972881fbd966e01891291d46fb
Daniel <outnumbered@live.com>
2020-02-28 22:32:39 +0000

    automated-commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * @argc: entry n
+ * @argv: entry array
  * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
