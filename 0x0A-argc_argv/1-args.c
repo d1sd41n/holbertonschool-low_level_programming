29124a7bab3440bcfb364b5b35f897bede43abe0
Daniel <outnumbered@live.com>
2020-02-28 22:40:02 +0000

    automated-commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: entry n
+ * @argv: entry array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
