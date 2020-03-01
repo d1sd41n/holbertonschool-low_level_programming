81a486a1f924fd245eb21c572d9ea6e8f1452697
Daniel Alejandro <outnumbered@live.com>
2020-03-01 17:37:36 -0500

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,26 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-int n, i, j;
+	int n, i, j;
 
-n = 0;
-if (argc <= 1)
-{
-printf("%i\n", n);
-return (1);
-}
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
- if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
-}
-}
-n += atoi(argv[i]);
-}
-printf("%i\n", n);
-return (0);
-}
+	n = 0;
+	if (argc <= 1)
+	{
+		printf("%i\n", n);
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		n += atoi(argv[i]);
+	}
+	printf("%i\n", n);
+	return (0);
+	}
