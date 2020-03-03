7765ff5ea6e9a4d24f13d537961e22f42dae6b04
Daniel <outnumbered@live.com>
2020-03-03 21:10:45 +0000

    automated-commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 && s1[i] != '\0'; i++)
 	;
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; s2 && s2[j]; j++)
 	;
 	j++;
 
@@ -32,6 +32,7 @@ char *str_concat(char *s1, char *s2)
 		s[i] = s2[j];
 		i++;
 	}
+	s[i] = '\0';
 	return (s);
 
 }
