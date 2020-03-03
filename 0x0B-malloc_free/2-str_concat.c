19d6e1a262c6336d2ba7b4e965684c6087327bcd
Daniel <outnumbered@live.com>
2020-03-03 21:05:22 +0000

    automated-commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,11 +23,11 @@ char *str_concat(char *s1, char *s2)
 	s = malloc(i + j);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 && s1[i]; i++)
 	{
 		s[i] = s1[i];
 	}
-	for (j = 0; s && s2[j]; j++)
+	for (j = 0; s2 && s2[j]; j++)
 	{
 		s[i] = s2[j];
 		i++;
