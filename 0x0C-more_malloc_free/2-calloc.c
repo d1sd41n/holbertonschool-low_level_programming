23d16eb90e8b3cdeab1a6aefe1f706881e6a8c49
Daniel <outnumbered@live.com>
2020-03-05 19:57:24 +0000

    automated-commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_calloc - writes nudscfdsmbers
+ * @nmemb: poewdwedinter
+ * @size: poieewnteeewewedr
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+		s[i] = 0;
+	return (s);
+}
