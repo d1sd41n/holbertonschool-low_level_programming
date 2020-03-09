04d067c176e3576f12a6b1fe271c75937ed2061e
Daniel <outnumbered@live.com>
2020-03-09 22:50:49 +0000

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - writes nudscfdsmbers
+ * @d: pointer
+ * @name: pointer
+ * @age: pointer
+ * @owner: pointer
+ * Return: none
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
