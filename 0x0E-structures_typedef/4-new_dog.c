29354c904683aa1bae41a78df6598cf8ec1b7882
Daniel Alejandro <outnumbered@live.com>
2020-03-09 23:06:56 -0400

    dwd

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * *new_dog - writes nudscfdsmbers
+ * @d: name
+ * @d: age
+ * @d: *owner
+ * Return: none
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * free_dog - writes nudscfdsmbers
+ * @d: pointer
+ * Return: none
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
