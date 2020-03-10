00a95962fdfe5803ab3f25bac50b1e162009bab5
Daniel Alejandro <outnumbered@live.com>
2020-03-09 23:21:22 -0400

    ssd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,35 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - writes nudscfdsmbers
+ * @str: pointer
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	;
+	s = malloc(i + 1);
+	if (s == NULL)
+		{
+		free(s);
+		return (NULL);
+		}
+	for (j = 0; j < i + 1; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
+
 /**
  * *new_dog - writes nudscfdsmbers
  * @d: name
@@ -9,4 +39,16 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *perro;
+
+	perro = malloc(sizeof(dog_t));
+	if (perro == NULL)
+	{
+		free(perro);
+		return (NULL);
+	}
+	perro->name = name;
+	perro->age = age;
+	perro->owner = owner;
+	return (perro);
 }
