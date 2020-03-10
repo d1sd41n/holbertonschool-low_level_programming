f7177eb40ebeac5a61fc7a819c5f30936b580c65
Daniel Alejandro <outnumbered@live.com>
2020-03-09 22:47:08 -0400

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,5 +14,8 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
