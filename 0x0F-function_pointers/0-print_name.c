b402772263f6f1fc834f0189adc87fb5739a3a76
Daniel <outnumbered@live.com>
2020-03-11 20:34:41 +0000

    automated-commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - writes nudscfdsmbers
+ * @name: pointer
+ * @f: pointer
+ * Return: none
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
