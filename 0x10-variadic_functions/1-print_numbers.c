1e98e7eddc477b7f4e39650bcd7a39ad5c8f9a0e
Daniel Alejandro <outnumbered@live.com>
2020-03-13 23:55:34 -0400

    To be is to be the value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints string in reverse
+ * @: pointer to string
+ * @separator: possad
+ * @n: passacs
+ * Return: none
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (!n)
+		return;
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(valist);
+}
