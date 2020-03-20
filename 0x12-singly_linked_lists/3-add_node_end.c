05f5a62ba4eb6232dc0293167e38132db2130f63
Daniel Alejandro <outnumbered@live.com>
2020-03-20 00:29:08 -0400

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,23 +7,26 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *node;
 	list_t *temp = *head;
+	list_t *node;
 	int i;
-printf("------------------------");
 
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
+
 	node->next = NULL;
 	node->str = strdup(str);
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	node->len = i;
-	printf("------------------------");
-	while (temp != NULL)
-		temp = temp->next;
-	temp->next = node;
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = node;
+	}
 	return (node);
-
 }
