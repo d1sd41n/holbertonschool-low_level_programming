bc0067808e949493a57334ff54f9074c94dfb3f7
Daniel Alejandro <outnumbered@live.com>
2020-03-19 22:01:17 -0400

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints string
+ * @h: pointer to string
+ * Return: none
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
