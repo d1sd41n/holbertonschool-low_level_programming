e1d053e5bf3aa0eb8654ecb775978f698486aa0f
Daniel Alejandro <outnumbered@live.com>
2020-03-24 19:59:41 -0400

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,11 +7,14 @@
 int pop_listint(listint_t **head)
 {
 	int i;
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
 	i = (*head)->n;
+	temp = *head;
 	*head = (*head)->next;
+	free(temp);
 	return (i);
 }
