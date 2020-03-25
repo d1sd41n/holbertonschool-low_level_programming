a271cbf456fdfd2372692bce264d6b54a7055828
Daniel Alejandro <outnumbered@live.com>
2020-03-25 00:58:16 -0400

    sdsd

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = (*head)->next;
 		return (1);
 	}
-	while (temp != NULL || i < index)
+	while (temp != NULL || i <= index)
 	{
 		if (i == index - 1)
 		{
