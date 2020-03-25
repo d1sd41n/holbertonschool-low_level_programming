37d4785974f457d8201b6ffe1e159d555f2d14e2
Daniel Alejandro <outnumbered@live.com>
2020-03-25 00:50:21 -0400

    10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * *delete_nodeint_at_index - pr*insert_nodein
+ * @head: pointer to string
+ * @index: poewrwerinter to strierweng
+ * Return: nonerewwee
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *temp = *head;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	while (temp != NULL)
+	{
+		if (i == index - 1)
+		{
+			node = temp->next;
+			temp->next = node->next;
+			return (1);
+		}
+		temp = temp->next;
+		i++;
+	}
+	return (-1);
+}
