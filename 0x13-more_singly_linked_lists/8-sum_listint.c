105811d6883e8ffe31df22505bd84e5f1abb7b5e
Daniel Alejandro <outnumbered@live.com>
2020-03-24 20:57:16 -0400

    7

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - pwwqwqrwdwqdints swdwdqtring
+ * @head: pwdwqointwwqdqer to wdwstrwewing
+ * @index: pwdwqointwwqdqer to wdwstrwewing
+ * Return: ndffdone
+ */
+int sum_listint(listint_t *head)
