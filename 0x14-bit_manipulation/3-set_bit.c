f1e5b93ff07ab176ba7bb0ec2f0e7a2b5b9b85b4
Daniel Alejandro <outnumbered@live.com>
2020-03-26 22:25:33 -0400

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- * get_bit - description
+ * set_bit - description
  * @n: description
  * @index: description
  * Return: description
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i, aux = 1;
 
 	if (index > 32)
 		return (-1);
 
 	for (i = 0; i < index; i++)
-		*n = *n << 1;
-	*n |= 1;
+		aux = aux << 1;
+	*n |= aux;
 	return (1);
 }
