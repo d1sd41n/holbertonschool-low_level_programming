69c907f2e89e6d515b0d40ecbb01a3732857183b
Daniel Alejandro <outnumbered@live.com>
2020-03-26 22:33:44 -0400

    4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * clear_bit - description
+ * @n: description
+ * @index: description
+ * Return: description
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i, aux = 1;
+
+	if (index > 32)
+		return (-1);
+
+	for (i = 0; i < index; i++)
+		aux = aux << 1;
+	*n &= ~aux;
+	return (1);
+}
