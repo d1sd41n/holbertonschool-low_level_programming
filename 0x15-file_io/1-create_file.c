b9236bf77f63a47f2d346854dae50f53496b97af
Daniel Alejandro <outnumbered@live.com>
2020-04-01 00:06:04 -0400

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,29 +7,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int leer, esc, i;
+	int leer, esc, i = 0;
 
-	if (filename == NULL)
-		return (-1);
+	for (i = 0; text_content && text_content[i]; i++)
+		;
 
 	leer = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (leer == -1)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0' && text_content != NULL; i++)
-		;
-	if (text_content == NULL)
-	{
-		i = 0;
-		text_content = "";
-	}
+	if (i > 0)
+		esc = write(leer, text_content, i);
 
-	esc = write(leer, text_content, i);
 	if (esc == -1)
-	{
-		close(leer);
 		return (-1);
-	}
+
 	close(leer);
 	return (1);
 }
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * main - description
+ * @argc: description
+ * @argv: description
+ * Return: description
+ */
+int main(int argc, char *argv[])
+{
+}
