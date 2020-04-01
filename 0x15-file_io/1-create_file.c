4beca10c9365898afed6108a23c819690a615885
Daniel Alejandro <outnumbered@live.com>
2020-03-31 22:28:00 -0400

    1#

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,11 @@ int create_file(const char *filename, char *text_content)
 
 	for (i = 0; text_content[i] != '\0' && text_content != NULL; i++)
 		;
+	if (text_content == NULL)
+	{
+		i = 0;
+		text_content = "";
+	}
 
 	esc = write(leer, text_content, i);
 	if (esc == -1)
