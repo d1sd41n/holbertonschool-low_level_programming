0beabf87428834df62129b7582d0ae2d7ae527b4
Daniel Alejandro <outnumbered@live.com>
2020-03-31 22:31:17 -0400

    1#

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	leer = open(filename, O_WRONLY | O_TRUNC | O_CREAT);
+	leer = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (leer == -1)
 		return (-1);
 
