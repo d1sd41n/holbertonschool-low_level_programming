b177ca6ed408931620e081824ce787fa8e8dd6b3
Daniel Alejandro <outnumbered@live.com>
2020-04-01 00:44:23 -0400

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,4 +7,33 @@
  */
 int main(int argc, char *argv[])
 {
+	int leer, esc;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+		exit(97);
+	}
+	if (argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		exit(98);
+	}
+	if (argv[2] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", argv[2]),
+		exit(99);
+	}
+	leer = open(argv[1], O_RDONLY);
+	if (leer == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	esc = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (esc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
