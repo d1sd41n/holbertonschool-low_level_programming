329e08f05c0449ec3b46188d48f0c44048842495
Daniel Alejandro <outnumbered@live.com>
2020-03-31 23:28:13 -0400

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,10 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	for (i = 0; text_content[i] != '\0' && text_content != NULL; i++)
 	;
-
-	if (text_content)
-		esc = write(leer, text_content, i);
-
+	esc = write(leer, text_content, i);
 	if (esc == -1)
 	{
 		close(leer);
