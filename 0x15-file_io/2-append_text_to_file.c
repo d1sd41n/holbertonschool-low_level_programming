9e42ffcfe69f4576d95ca79f46a8d60e40c09972
Daniel Alejandro <outnumbered@live.com>
2020-03-31 23:26:41 -0400

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,27 @@
 #include "holberton.h"
 /**
- * create_file - description
+ * append_text_to_file - description
  * @filename: description
  * @text_content: description
  * Return: description
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int leer, esc, i;
+	int leer, esc, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	leer = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	leer = open(filename, O_WRONLY | O_APPEND);
 	if (leer == -1)
 		return (-1);
 
 	for (i = 0; text_content[i] != '\0' && text_content != NULL; i++)
-		;
-	if (text_content == NULL)
-	{
-		i = 0;
-		text_content = "";
-	}
+	;
 
-	esc = write(leer, text_content, i);
+	if (text_content)
+		esc = write(leer, text_content, i);
+
 	if (esc == -1)
 	{
 		close(leer);
