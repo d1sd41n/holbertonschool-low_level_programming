cf9b3dc96db0f75f3b04ee1be75f68f9784ef1a8
Daniel Alejandro <outnumbered@live.com>
2020-04-01 00:11:15 -0400

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,26 +9,19 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int leer, esc, i = 0;
 
-	if (filename == NULL)
-		return (-1);
-
-	leer = open(filename, O_WRONLY | O_APPEND);
+	leer = open(filename, O_APPEND | O_WRONLY);
 	if (leer == -1)
 		return (-1);
 
 	for (i = 0; text_content[i] != '\0' && text_content != NULL; i++)
-	;
+		;
 
-	if (text_content)
-	{
+	if (i > 0)
 		esc = write(leer, text_content, i);
 
-		if (esc == -1)
-		{
-			close(leer);
-			return (-1);
-		}
-	}
+	if (esc == -1)
+		return (-1);
+
 	close(leer);
 	return (1);
 }
