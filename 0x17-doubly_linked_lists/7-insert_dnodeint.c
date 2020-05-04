bd961a50b0567ae08dbe04223a7758afd7371a70
Daniel Alejandro <outnumbered@live.com>
2020-05-03 21:33:55 -0500

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,6 +30,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
 			node->prev = temp;
 			node->next = temp->next;
 			temp->next->prev = node;
