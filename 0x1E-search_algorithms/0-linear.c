52ae151cbc57bd7b664f3f7ce0945f5d67a13d6d
Daniel Alejandro <outnumbered@live.com>
2020-08-25 04:52:30 -0500

    0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - docs  fo the function¿
+ * @array: array of int
+ * @size: int
+ * @value: int
+ *
+ * Return: Nothing.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
